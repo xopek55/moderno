75f76c28601a89f74ea8b0767023d554cc43c84d
xopek55 <xopek55@gmail.com>
2020-03-10 10:27:07 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
 
         $(".rate-star").rateYo({
@@ -8,11 +8,32 @@ $(function(){
         });
 
         $('.product-slider__inner').slick({
-               dots: true,
-               arrows: false,
-               slidesToShow: 4,
-               slidesToScroll: 4,
-        });    
+                dots: true,
+                arrows: false,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+        });
 
-var mixer = mixitup('.products__inner-box');
+        $(".js-range-slider").ionRangeSlider({
+                type: "double",
+                min: 0,
+                max: 1000,
+                from: 0,
+                to: 600,
+                prefix: "$"
+        });
+
+        $('.icon-th-list').on('click', function(){
+               $('.product__item').addClass('list');
+                $('.icon-th-list').addClass('active');
+                $('.icon-th-large').removeClass('active');
+        });
+
+        $('.icon-th-large').on('click', function (){
+                $('.product__item').removeClass('list');
+                $('.icon-th-large').addClass('active');
+                $('.icon-th-list').removeClass('active');
+        });
+
+        var mixer = mixitup('.products__inner-box');
 });
